0ecb266565478ce8181f67cab54f17ddec7c4d86
lpy <295204700@qq.com>
2021-02-02 13:58:01 +0800

    准备提交

--- src/com/sy/HelloWorld02.java
+++ src/com/sy/HelloWorld02.java
@@ -1,2 +1,10 @@
+package com.sy;
 public class HelloWorld02 {
+    public static void main(String[] args) {
+        System.out.println("上元教育");
+        System.out.println("上元教育master");
+        System.out.println("上元教育master2");
+        System.out.println("上元教育master3");
+        System.out.println("上元教育dev03");
+    }
 }
